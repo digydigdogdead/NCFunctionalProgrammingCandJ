0a29046495e31c0ff5b5b77187f82742d2d5a965
Jason Hunter <Jason.Hunter@computastar.com>
2024-11-22 13:58:02 +0000

    Challange 7 - Func GetCoolPeople

---
+++ FunctionalProgramming/Exercises002.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class Exercises002
+    {
+        public static Func<List<string>> GetCoolPeople = () => new List<string>
+        {
+        "rich.neat@boardgamer.com",
+        "poppy.mcdonnell@irishdancer.com",
+        "neil.hughes@walkingoncustard.com",
+        "alice.yang@midfielder.com",
+        "pippa.austin@musician.com"
+        };
+
+
+
+    }
+}
